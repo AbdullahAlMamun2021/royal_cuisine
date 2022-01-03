2a597f0e0c8151118725ace0c8083451ff66fff9
AbdullahAlMamun2021 <92459757+AbdullahAlMamun2021@users.noreply.github.com>
2022-01-03 11:39:28 +0600

    first commit

---
+++ lib/cart.dart
@@ -0,0 +1,45 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:royal_cuisine/constants.dart';
+
+import 'Screens/first_screen.dart';
+
+
+class Cart extends StatefulWidget {
+  @override
+  _CartState createState() => _CartState();
+}
+
+class _CartState extends State<Cart> {
+  int _currentIndex=0;
+  final ttabs=[
+    FirstScreen(),
+    Center(child: Text('home'),),
+    Center(child: Text('home'),),
+  ];
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+          backgroundColor: Colors.black,
+      body:ttabs[_currentIndex],
+     bottomNavigationBar: Container(
+       height: 60,
+       color:greenColor.withOpacity(0.2) ,
+     child: Row(children: [
+       SizedBox(width: 15,),
+       Text('1 items ',style: TextStyle(color:greenColor,fontSize: 18),),
+       Icon(Icons.circle_rounded,size: 10,color: greenColor,),
+       Text(' ৳ 80.00',style: TextStyle(color:greenColor,fontSize: 18)),
+       SizedBox(width: 120,),
+       FlatButton(
+         shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8.0)),
+         color: greenColor,
+         onPressed: (){}, child: Text("View cart",style: TextStyle(fontWeight: FontWeight.w600,fontSize: 18),))
+     ],),
+     ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:royal_cuisine/Screens/first_screen.dart';
+import 'package:royal_cuisine/cart.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,16 +13,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        initialRoute: "/",
+      routes: {
+        "/Second":(context)=>Cart(),
+      },
       title: 'Flutter Demo',
       theme: ThemeData(
         appBarTheme: AppBarTheme(
-          
+          color: Colors.black
         ),
         scaffoldBackgroundColor: Colors.black,
         primarySwatch: Colors.green
       ),
       debugShowCheckedModeBanner: false,
-      home: FirstScreen(),
+      home: Cart(),
     );
   }
 }
